6472cbd5c3f2298994c05b6e3f16e4355b3eea7c
Sagentoid <46150703+sagentoid@users.noreply.github.com>
2019-01-16 21:51:28 +0900

    Add files via upload

--- js/convert.js
+++ js/convert.js
@@ -1,13 +1,27 @@
+function enter() 
+{
+	// Get the input field
+var input = document.getElementById("num");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) 
+	{
+  // Cancel the default action, if needed
+  event.preventDefault();
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Trigger the button element with a click
+    document.getElementById("myBtn").click();
+  }
+	});
+}
+
 function validate()
 {
 var x = document.getElementById("num").value;
 if (x == "")
 	{
 	alert("You haven't entered anything");
-	}
-else if	(x = isNaN(x))
-	{
-	alert("Entry must be a whole numerical value");
 	}
 else
 	{
