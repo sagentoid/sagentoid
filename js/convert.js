e76dbf943156a1000fde8487e0075f3c7edb2960
Sagentoid <46150703+sagentoid@users.noreply.github.com>
2019-10-15 12:16:38 +0900

    Add files via upload

--- js/convert.js
+++ js/convert.js
@@ -1,54 +1,55 @@
 function enter() 
 {
-	// Get the input field
-var input = document.getElementById("num");
+	
+	var input = document.getElementById("num"); // Get the input field
 
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) 
-	{
-  // Cancel the default action, if needed
-  event.preventDefault();
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Trigger the button element with a click
-    document.getElementById("myBtn").click();
-  }
+	input.addEventListener("keyup", function(event)	// Execute a function when the user releases a key on the keyboard
+	{ 
+
+		event.preventDefault();   // Cancel the default action, if needed
+	  
+		if (event.keyCode === 13) {   // Keycode "13" is the "Enter" key on the keyboard
+	
+			document.getElementById("myBtn").click(); // Trigger the button element with a click
+
+		}
 	});
 }
 
 function validate()
 {
 var x = document.getElementById("num").value;
-if (x == "")
+	
+	if (x == "")
 	{
-	alert("You haven't entered anything");
+		alert("You haven't entered anything");
 	}
-else
+	else
 	{
-	monsToYrs();
+		monsToYrs();
 	}
 }
 
 
 
-function monsToYrs()
+function monsToYrs() 
 {
 
-var x=document.getElementById("num").value;
-var y=x%12;
-var z=Math.floor(x/12);
-var answer=document.getElementById("answerDisp");
-var mon = "month.";
-var yr = "year";
-if (y == 0 || y >= 2)
+	var x=document.getElementById("num").value;
+	var y=x%12;
+	var z=Math.floor(x/12);
+	var answer=document.getElementById("answerDisp");
+	var mon = "month.";
+	var yr = "year";
+		if (y == 0 || y >= 2) 
 	{
-	var mon = "months.";
-	}
-if (z == 0 || z >= 2)
+			var mon = "months.";
+	}	
+		if (z == 0 || z >= 2) 
 	{
-	var yr = "years";
+			var yr = "years";
 	}
 
-answerDisp.innerHTML = "The number above converts to " + z + " " + yr + " and " + parseInt(y) + " " + mon;
+	answerDisp.innerHTML = "The number above converts to " + z + " " + yr + " and " + parseInt(y) + " " + mon;
 
 }
